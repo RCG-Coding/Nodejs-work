b67b702910121dfc431ca0ff2253cfd673ff21f5
Roshan Chathuranga <147641397+Roshan-Coding@users.noreply.github.com>
2024-03-03 10:43:12 +0530

    create a server in nodejs

--- node.js
+++ node.js
@@ -1,6 +1,39 @@
+const http = require('http');
 const fs = require('fs');
 
-const rf = fs.readFileSync("./files/input.txt", "utf-8");
-console.log(rf);
+const server = http.createServer((req, res) => {
+    let path = req.url;
+    const html = fs.readFileSync('./files/my.html', 'utf-8');
 
-fs.writeFileSync("./files/output.txt", `Roshan's informations \n\n ${rf}`);
+    if(path.toLocaleLowerCase() === '/' || path.toLocaleLowerCase() === '/home'){
+        res.writeHead(200,{
+            "content-type" : "text/html",
+            "Document": "my-first-document"       
+        })
+        res.end(html.replace('{{%CONTENT%}}', 'you are in the HOME page now'));
+    }else if(path.toLocaleLowerCase() === '/login'){
+        res.writeHead(200,{
+            "content-type" : "text/html",
+            "Document": "my-first-document"       
+        })
+        res.end(html.replace('{{%CONTENT%}}', 'you are in the LOGIN page now'));
+    }else if(path.toLocaleLowerCase() === '/register'){
+        res.writeHead(200,{
+            "content-type" : "text/html",
+            "Document": "my-first-document"       
+        })
+        res.end(html.replace('{{%CONTENT%}}', 'you are in the REGISTER page now'));
+    }else{
+        res.writeHead(404,{
+            "content-type" : "text/html",
+            "Document": "my-first-document"       
+        })
+        res.end(html.replace('{{%CONTENT%}}', 'Error 404 : The page is not FOUND'));
+    }
+    console.log("got a one request from a client");
+});
+
+server.listen(2000,() => {
+    console.log("App is running on port 2000")
+});
+
